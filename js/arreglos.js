59305192b30416d1a38fa6e1fc715d75ed379275
Edward <evelasquez0@ucol.mx>
2026-02-18 07:56:36 -0600

    Added even and odd.

---
+++ js/arreglos.js
@@ -0,0 +1,14 @@
+let numbers = [];
+let even = [];
+let odd = [];
+
+for (let i; i < 50; i++)
+    numbers.push(i + 1);
+
+numbers.forEach((number) => {
+    if (number % 2 === 0) {
+        even.push(number)
+    } else {
+        odd.push(odd)
+    }
+})
